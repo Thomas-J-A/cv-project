768776b7fd250144e14baae03ee7ff1e8a284e3c
Thomas Aspbury <tomaspbury@icloud.com>
2021-08-06 10:58:21 +0100

    Finish setting up project

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/reso:urce',
+        generator: {
+          filename: 'images/[name].[ext]',
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    publicPath: 'http://localhost:8080/dist/',
+    openPage: 'dist/',
+    hot: true,
+    compress: true,
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      // favicon: './src/favicon.ico',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  // devtool: 'eval-cheap-source-map',
+};
